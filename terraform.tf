420f3f47de179fa9407739cf08645298171821ca
fex <git@fexmail.de>
2023-01-18 18:33:30 +0100

    initial commit

---
+++ terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+
+  cloud {
+    organization = "miske-learn"
+
+    workspaces {
+      name = "learn-terraform-cloud"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 0.14.0"
+}
